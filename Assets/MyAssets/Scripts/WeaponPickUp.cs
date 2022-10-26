25b2e90f82b333fc1c41ab87b5913092c7e7e7ea
NW_DLT <alexputil228@mail.ru>
2022-10-26 12:35:02 +0300

    add_weapon_swap

---
+++ Assets/MyAssets/Scripts/WeaponPickUp.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickUp : MonoBehaviour
+{
+    public Camera camera;
+    public float distance = 2f;
+    GameObject currentWeapon;
+    bool canPickUp;
+
+    public static Action<GameObject> HandWeaponUse;
+    public static Action<Camera> WeaponWasPickUp;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E)) PickUp();
+        if(Input.GetKeyDown(KeyCode.Q)) Drop();
+        //if(Input.GetKeyDown(KeyCode.Mouse0) && currentWeapon != null) HandWeaponUse(currentWeapon);
+        if (Input.GetButton("Fire1"))
+        {
+            HandWeaponUse(currentWeapon);
+        }
+    }
+
+    void PickUp()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(camera.transform.position, camera.transform.forward,out hit, distance))
+        {
+            if(hit.transform.tag == "Weapon")
+            {
+                if (canPickUp) Drop();
+
+                currentWeapon = hit.transform.gameObject;
+                currentWeapon.GetComponent<Rigidbody>().isKinematic = true;
+                currentWeapon.transform.parent = transform;
+                currentWeapon.transform.localPosition = Vector3.zero;
+                currentWeapon.transform.localEulerAngles = new Vector3(5f, 180f, 0f);
+                canPickUp = true;
+                WeaponWasPickUp(camera);
+            }
+        }
+    }
+
+    void Drop()
+    {
+        if(currentWeapon != null)
+        {
+            currentWeapon.transform.parent = null;
+            currentWeapon.GetComponent<Rigidbody>().isKinematic = false;
+            currentWeapon.GetComponent<Rigidbody>().AddForce(1, 1, 1.0f, ForceMode.Impulse);
+            canPickUp = false;
+            currentWeapon = null;
+        }
+    }
+}
--- Assets/WeaponShoot.cs
+++ Assets/WeaponShoot.cs
@@ -5,28 +5,38 @@ using UnityEngine;
 
 public class WeaponShoot : MonoBehaviour
 {
-    public float damage = 1f;
-    public float fireRate = 20f;
-    public float range = 15f;
-    public float force = 100f;
+    [SerializeField] float damage;
+    public float fireRate;
+    public float range;
+    [SerializeField] int bullets_count;
+    //public float force;
     public ParticleSystem muzzleFlash;
     public Transform bulletSpawn;
     public AudioClip shotSFX;
     public AudioSource _audioSource;
     public GameObject hitEffect;
 
-    public Camera _cam;
+    private Camera _cam;
     private float nextFire = 0f;
 
     public static Action<GameObject> OnGameObjectClick;
 
-    private void Start()
+    private void OnEnable()
     {
+        WeaponPickUp.HandWeaponUse += tryShoot;
+        WeaponPickUp.WeaponWasPickUp += set_cam;
     }
-    // Update is called once per frame
-    void Update()
+
+    private void set_cam(Camera cam)
     {
-        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        this._cam = cam;
+    }
+
+    // Update is called once per frame
+    void tryShoot(GameObject gun)
+    {
+        Debug.Log("Bullets: " + this.bullets_count);
+        if (Time.time > nextFire && gun == this.gameObject && bullets_count > 0)
         {
             nextFire = Time.time + 1f / fireRate;
             Shoot();
@@ -35,24 +45,27 @@ public class WeaponShoot : MonoBehaviour
 
     void Shoot()
     {
-        _audioSource.PlayOneShot(shotSFX);
-        muzzleFlash.Play(); 
+        
+            _audioSource.PlayOneShot(shotSFX);
+            muzzleFlash.Play();
 
-        RaycastHit hit;
+            RaycastHit hit;
 
-        if (Physics.Raycast(_cam.transform.position, _cam.transform.forward,out hit,range))
-        {
-            //Debug.Log("Попал " + hit.collider);
+            if (Physics.Raycast(_cam.transform.position, _cam.transform.forward, out hit, range))
+            {
+                //Debug.Log("Попал " + hit.collider);
 
-            GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(impact, 0.1f);
-            OnGameObjectClick(hit.transform.gameObject);
-       
-        }
-        else
-        {
-            ScoreViewer.instance.RemovePoint();
-        }
+                GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impact, 0.1f);
+                OnGameObjectClick(hit.transform.gameObject);
+
+            }
+            else
+            {
+                ScoreViewer.instance.RemovePoint();
+            }
+            this.bullets_count -= 1;
+
     }
 
 }
